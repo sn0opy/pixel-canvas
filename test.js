cb35edf258e97631a90ac40b4ebbc42a5bddab0a
Sascha Ohms <sasch9r@gmail.com>
2013-01-18 16:18:16 +0100

    test.js fix

--- test.js
+++ test.js
@@ -5,11 +5,12 @@ var area_height = 100;
 var area_width = 100;
 
 for(var i = 1; i <= count; i++) {
-	var x = Math.floor(Math.random() * area_width);
-	var y = Math.floor(Math.random() * area_height);
-	var color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
-	
-	client = net.connect({port: 4001}, function() { 	
+	client = net.connect({port: 4001}, function() {
+		var x = Math.floor(Math.random() * area_width);
+		var y = Math.floor(Math.random() * area_height);
+		var color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+		
 		client.write(x + ','+ y + ',' + color);
+		console.log(x + ','+ y + ',' + color);
 	});
 }
